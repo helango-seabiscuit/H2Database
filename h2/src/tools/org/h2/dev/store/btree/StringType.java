f2f9f3e67bc52587f6e616b3264e21a92947027c
hemalatha <hemalatha@squaretrade.com>
2013-07-07 15:43:14 -0700

    Adding H2Databse open source project

---
+++ h2/src/tools/org/h2/dev/store/btree/StringType.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2004-2011 H2 Group. Multiple-Licensed under the H2 License,
+ * Version 1.0, and under the Eclipse Public License, Version 1.0
+ * (http://h2database.com/html/license.html).
+ * Initial Developer: H2 Group
+ */
+package org.h2.dev.store.btree;
+
+import java.nio.ByteBuffer;
+
+/**
+ * A string type.
+ */
+public class StringType implements DataType {
+
+    public int compare(Object a, Object b) {
+        return a.toString().compareTo(b.toString());
+    }
+
+    public int getMaxLength(Object obj) {
+        return DataUtils.MAX_VAR_INT_LEN + 3 * obj.toString().length();
+    }
+
+    public int getMemory(Object obj) {
+        return 24 + 2 * obj.toString().length();
+    }
+
+    public String read(ByteBuffer buff) {
+        int len = DataUtils.readVarInt(buff);
+        return DataUtils.readString(buff, len);
+    }
+
+    public void write(ByteBuffer buff, Object obj) {
+        String s = obj.toString();
+        int len = s.length();
+        DataUtils.writeVarInt(buff, len);
+        DataUtils.writeStringData(buff, s, len);
+    }
+
+    public String asString() {
+        return "";
+    }
+
+}
+
